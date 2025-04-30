ef5e5baa60fb5f5af3adf9be53123971415c035d
Lizavete-Moisa <sliza1882@gmail.com>
2025-04-30 15:35:19 +0300

    refactor Hello class

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
-class Hello{
-public static void main(String[] args){
-  System.out.println("Hello world!");
-}
+class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("Число а: " + a);
+        System.out.println("Hello world!");
+    }
 }
